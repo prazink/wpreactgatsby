58614cefbf6c6849f645d06e4c8ef652f41ef54b
PrazinK <prazink@gmail.com>
2019-06-30 15:19:48 +1000

    Headless CMS Gatsby site Ninth commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -13,7 +13,7 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage, createRedirect } = actions
   createRedirect({
     fromPath: "/",
-    toPath: "/",
+    toPath: "/home",
     redirectInBrowser: true,
     isPermanent: true,
   })
